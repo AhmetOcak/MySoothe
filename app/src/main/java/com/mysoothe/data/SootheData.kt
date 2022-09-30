f42ed5b0d6b4c50d409d54d72b217e1d37d48ad6
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-09-30 21:20:00 +0300

    app data ready

---
+++ app/src/main/java/com/mysoothe/data/SootheData.kt
@@ -0,0 +1,39 @@
+package com.mysoothe.data
+
+import com.mysoothe.R
+import com.mysoothe.data.models.CardItem
+
+object SootheData {
+
+    val favoriteCollections = listOf(
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir")
+    )
+
+    val alignYourBody = listOf(
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir")
+    )
+
+    val alignYourMind = listOf(
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir"),
+        CardItem(R.drawable.anitkabir, "Anıtkabir")
+    )
+
+    val navItems = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Profile
+    )
+}
