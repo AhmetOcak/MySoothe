10b56e81d031c6295e5d9e4e8fe7f93c6f05bcba
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-09-30 21:13:13 +0300

    app theme end

--- app/src/main/java/com/mysoothe/ui/theme/Color.kt
+++ app/src/main/java/com/mysoothe/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package com.mysoothe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Gray900 = Color(0xFF333333)
+val Gray800 = Color(0xCC333333)
+val Rust600 = Color(0xFF886363)
+val Rust300 = Color(0xFFE1AFAF)
+val Taupe100 = Color(0xFFF0EAE2)
+val Taupe800 = Color(0xFF655454)
+val White850 = Color(0xD9FFFFFF)
+val White800 = Color(0xCCFFFFFF)
+val White150 = Color(0x26FFFFFF)
+val White = Color(0xFFFFFFFF)
+
--- app/src/main/java/com/mysoothe/ui/theme/Theme.kt
+++ app/src/main/java/com/mysoothe/ui/theme/Theme.kt
@@ -1,38 +1,47 @@
 package com.mysoothe.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = White,
+    secondary = Rust300,
+    background = Gray900,
+    surface = White150,
+    onPrimary = Gray900,
+    onSecondary = Gray900,
+    onBackground = Taupe100,
+    onSurface = White800
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Gray900,
+    secondary = Rust600,
+    background = Taupe100,
+    surface = White850,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = Taupe800,
+    onSurface = Gray800
 )
 
 @Composable
 fun MySootheTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+    val colors: Colors
+    val systemUiController = rememberSystemUiController()
+
+    if (darkTheme) {
+        colors = DarkColorPalette
+        systemUiController.setStatusBarColor(color = Color.Transparent, darkIcons = false)
     } else {
-        LightColorPalette
+        colors = LightColorPalette
+        systemUiController.setStatusBarColor(color = Color.Transparent, darkIcons = true)
     }
 
     MaterialTheme(
